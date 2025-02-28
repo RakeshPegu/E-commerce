4d204e61cb2fd2b201b9e157388e730f41592b52
RakeshPegu <rakeshpegu903@gmail.com>
2025-02-28 14:24:49 +0530

    changes in package file

--- server/server.js
+++ server/server.js
@@ -4,8 +4,10 @@ import cookieParser from 'cookie-parser'
 import productRoute from './routes/product.js'
 import userRoute from './routes/user.js'
 import cors from 'cors'
+
 const app = express()
 const port = process.env.PORT || 5000;
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin: process.env.CLIENT_URL , credentials:true}))
